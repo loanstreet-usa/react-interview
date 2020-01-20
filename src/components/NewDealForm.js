1f398cacbe2999bfb1681bc70fca8a4871f403a6
Elaine <eyw2008@gmail.com>
2020-01-20 17:56:26 -0500

    Implemented validation & error messages on 3 form fields.

--- src/components/NewDealForm.js
+++ src/components/NewDealForm.js
@@ -11,6 +11,11 @@ const DEFAULT_DEAL = {
   dealSize: '',
   isPublished: false
 };
+const DEFAULT_ERRORS = {
+  institution: null,
+  dealType: null,
+  dealSize: null
+}
 
 class DealForm extends Component {
   static propTypes = {
@@ -22,19 +27,43 @@ class DealForm extends Component {
   }
 
   // State represents a deal.
-  state = { ...DEFAULT_DEAL };
+  state = { deal: { ...DEFAULT_DEAL }, errors: { ...DEFAULT_ERRORS } };
 
-  propertyUpdater(property) {
-    return e => this.setState({[property]: e.target.value});
+  propertyUpdater(e, property) {
+    console.log("trigger change")
+    if (this.state.errors[property] !== null) {
+      this.setState({ errors: { ...this.state.errors, [property]: null } })
+    }
+    this.setState({ deal: { ...this.state.deal, [property]: e.target.value } });
   }
+
+
 
   createDeal = e => {
     e.preventDefault();
-    if (this.props.onCreateDeal)
-      this.props.onCreateDeal({ ...this.state });
-
-    // Reset state for the next deal input.
-    this.setState({ ...DEFAULT_DEAL });
+    const errors = { ...DEFAULT_ERRORS };
+    let cleanedDealSize;
+    const { institution, dealType, dealSize } = this.state.deal;
+    if (institution.length === 0) {
+      errors.institution = "Please enter an institution name."
+    }
+    if (dealType.length === 0) {
+      errors.dealType = "Please enter a deal type."
+    }
+    if (dealSize.length === 0) {
+      errors.dealSize = "Please enter a deal size."
+    } else {
+      cleanedDealSize = dealSize.split("").filter((x) => x !== "$" && x !== ",").join("");
+      if (isNaN(cleanedDealSize + 1)) {
+        errors.dealSize = "That is not a valid deal amount."
+      }
+    }
+    this.setState({ errors })
+    if (Object.keys(errors).every((key) => errors[key] === null)) {
+      this.props.onCreateDeal({ ...this.state.deal, dealSize: cleanedDealSize });
+      // Reset state for the next deal input.
+      this.setState({ deal: { ...DEFAULT_DEAL } });
+    }
   }
 
   render() {
@@ -45,36 +74,48 @@ class DealForm extends Component {
             <input
               className="NewDealForm--input"
               ref="institution"
-              value={this.state.institution}
+              value={this.state.deal.institution}
               placeholder="LS Credit Union"
-              onChange={this.propertyUpdater('institution')}
+              onChange={(e) => this.propertyUpdater(e, 'institution')}
               required
             />
           </label>
+
+        </div>
+        <div>
+          <label className="NewDealForm--error">{this.state.errors.institution}</label>
         </div>
         <div>
           <label className="NewDealForm--label">Deal Type:
             <input
               className="NewDealForm--input"
               ref="dealType"
-              value={this.state.dealType}
+              value={this.state.deal.dealType}
               placeholder="Consumer Auto"
-              onChange={this.propertyUpdater('dealType')}
+              onChange={(e) => this.propertyUpdater(e, 'dealType')}
               required
             />
           </label>
+
+        </div>
+        <div>
+          <label className="NewDealForm--error">{this.state.errors.dealType}</label>
         </div>
         <div>
           <label className="NewDealForm--label">Deal Size:
             <input
               className="NewDealForm--input"
               ref="dealSize"
-              value={this.state.dealSize}
+              value={this.state.deal.dealSize}
               placeholder="$1,000,000"
-              onChange={this.propertyUpdater('dealSize')}
+              onChange={(e) => this.propertyUpdater(e, 'dealSize')}
               required
             />
           </label>
+
+        </div>
+        <div>
+          <label className="NewDealForm--error">{this.state.errors.dealSize}</label>
         </div>
         <button className="NewDealForm--button" onClick={this.createDeal}>Create Deal</button>
       </form>
